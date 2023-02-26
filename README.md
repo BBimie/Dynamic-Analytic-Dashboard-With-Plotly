71f120191fe964516ab84c12bbd922c925ba7252
BimboBabatola <bimbo@woodcoreapp.com>
2023-02-26 19:03:43 +0100

    setup source  database

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Dynamic-Analytic-Dashboard-With-Plotly
 A dynamic dashboard built with plotly, the data is stored in a database and the webapp is built with FastAPI
+
+
+- CSV data > SQL database (with maybe ORM?)  > queries to get the data out > data rendered into plotly dashboard
--- app.py
+++ app.py
@@ -3,9 +3,21 @@ import pandas as pd
 import json
 import plotly
 import plotly.express as px
+from src.data.database.connection import Database
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'crud.sqlite')
+# db = SQLAlchemy(app)
+# ma = Marshmallow(app)
 
 app = Flask(__name__)
 
-app.route('/')
-def index():
-    return render_template('dashboard.html')
+@app.route('/', methods=['GET', 'POST'])
+def entrypoint():
+    pass
+
+def get_data():
+    pass
+
+
+if __name__ == '__main__':
+    app.run()
