cab50388d079ed3f7f5c1c2ac9067d500a9c9c66
BimboBabatola <bimbo@woodcoreapp.com>
2023-06-02 00:56:37 +0100

    extract

--- src/extract/handler.py
+++ src/extract/handler.py
@@ -1,15 +1,34 @@
-from data.database.connection import Database
-from query import Top10Products
+from src.data.database.connection import Database
+from src.extract.query import ExtractionQueries, FilterQueries
 import pandas as pd
 
-class GenerateData:
-    def __init__(self) -> None:
-        pass
 
-    def top_products(self): 
+class FilterValues:
+    def __init__(self) -> None:
+        self.conn = self.conn = Database(db_name='ecommerce').connect()
+
+    def year_filter(self) -> list:
+        sql = FilterQueries().year_filter_script()
+        df = pd.read_sql(sql, self.conn)
+        years:list = list(df['year'])
+        return years
+
+
+class GenerateData:
+    def __init__(self, year) -> None:
+        #self.product = product
+        self.year = year
+        if year != None:
+            self.yearClause = f" WHERE strftime('%Y', order_purchase_timestamp) =  '{self.year}' "
+        else:
+            self.yearClause = ""
+        self.conn = Database(db_name='ecommerce').connect()
+
+
+    def top_products(self) -> pd.DataFrame: 
         DATA = []
-        sql = Top10Products().script()
-        for data in pd.read_sql(sql, Database(db_name='ecommerce').connect(), chunksize=10):
+        sql = ExtractionQueries(yearClause=self.yearClause).top_products_script()
+        for data in pd.read_sql(sql, self.conn, chunksize=10):
             DATA.append(data)
         df = pd.concat(DATA)
         try:
@@ -17,5 +36,22 @@ class GenerateData:
             return df
         except:
             return df
+        
+    def total_orders(self) -> int:
+        sql = ExtractionQueries(yearClause=self.yearClause).total_order_script()
+        print('orders sql', sql)
+        df = pd.read_sql(sql, self.conn)
+        orders = df['orders'][0]
+        format_number = lambda x: f"{x/1000:.2f}K" if x < 1_000_000 else f"{x/1_000_000:.2f}M"
+        formatted_orders = format_number(orders)
+        return formatted_orders
+    
+    def total_sales(self) -> float:
+        sql = ExtractionQueries(yearClause=self.yearClause).total_sales_script()
+        df = pd.read_sql(sql, self.conn)
+        sales = df['sales'][0]
+        format_number = lambda x: f"{x/1000:.2f}K" if x < 1_000_000 else f"{x/1_000_000:.2f}M"
+        formatted_sales = format_number(sales)
+        return formatted_sales
 
 
--- src/extract/query.py
+++ src/extract/query.py
@@ -1,49 +1,42 @@
 
-class Top10Products:
+class FilterQueries:
     def __init__(self) -> None:
         pass
 
-    def script() -> str:
-        return """ SELECT pt.product_category_name_english AS product_name, 
+    def year_filter_script(self):
+        return """SELECT DISTINCT strftime('%Y', order_purchase_timestamp) AS year FROM orders """
+
+
+class ExtractionQueries:
+    def __init__(self, yearClause) -> None:
+        self.yearClause = yearClause
+
+    def top_products_script(self) -> str:
+        return f""" SELECT pt.product_category_name_english AS product_name, 
                             COUNT(oi.product_id) AS 'number_ordered'  FROM order_items oi
                     LEFT JOIN products p on p.id = oi.product_id
                     LEFT JOIN product_category_name_translation pt ON p.category_name = pt.product_category_name
+                    LEFT JOIN orders o ON o.id = oi.order_id
+                    { self.yearClause }
                     GROUP BY p.category_name
                     ORDER BY 2 DESC 
                     LIMIT 10 """
 
 
-class TotalSales:
-    def __init__(self) -> None:
-        pass
+    def total_sales_script(self) -> str:
+        return """ SELECT SUM(payment_value) AS 'sales' FROM order_payments op 
+                    LEFT JOIN orders o ON o.id = op.order_id  """
 
-    def script() -> str:
-        pass
-
-class TotalProfits:
-    def __init__(self) -> None:
-        pass
-
-    def script() -> str:
-        pass
-
-class TotalOrders:
-    def __init__(self) -> None:
-        pass
-
-    def script() -> str:
-        pass
-
-class productData:
-    def __init__(self) -> None:
-        pass
-
-    def script() -> str:
-        pass
-
-class productData:
-    def __init__(self) -> None:
-        pass
-
-    def script() -> str:
-        pass
+    def total_order_script(self) -> str:
+        return f""" SELECT COUNT(id) AS 'orders' FROM orders
+                    {self.yearClause}  """
+    
+    def total_delivered_order_script(self) -> str:
+        return """ SELECT COUNT(id) AS 'orders' FROM orders WHERE order_status = 'delivered' """
+    
+    def number_products_sold() -> str:
+        return """ """
+    
+    
+    def order_growth_script(self) -> str:
+        return """  """
