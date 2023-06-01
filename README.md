f78b9a132f4cf0092e62a1024f5969396cebd853
BimboBabatola <bimbo@woodcoreapp.com>
2023-06-02 00:57:38 +0100

    latest update

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ A dynamic dashboard built with plotly, the data is stored in a database and the 
 
 
 - CSV data > SQL database (with maybe ORM?)  > queries to get the data out > data rendered into plotly dashboard
+
+
+Note that the |safe filter is used to mark the JSON data as safe for rendering in the HTML template. This is important for security reasons.
+
+With these steps, you should be able to render multiple Plotly graphs in an HTML file using Flask.
--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import json
 import plotly
 import plotly.express as px
 from src.data.database.connection import Database
-from src.extract.handler import GenerateData
+from src.extract.handler import GenerateData, FilterValues
+import plotly
+import plotly.graph_objs as go
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'crud.sqlite')
 # db = SQLAlchemy(app)
@@ -12,15 +14,45 @@ from src.extract.handler import GenerateData
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def entrypoint():
-    pass
+@app.route('/her', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        year = request.form.get('year')
+        product_json = top_product_data(year=year).to_json()
+        return render_template('dashboard.html', filters=dashboard_filters(), product_plot=product_json, card=card_data(year=year))
+    else:
+        year = None
+        product_json = top_product_data(year=year).to_json()
+        return render_template('dashboard.html', filters=dashboard_filters(), product_plot=product_json, card=card_data(year=year))
+    
+    
 
-def top_product_data():
-    df = GenerateData().top_products()
+
+# @app.route('/', methods=['GET', 'POST'])
+# def dashboard():
+#     year = request.form.get('year')
+#     product_json = top_product_data(year=year).to_json()
+#     return render_template('dashboard.html', filters=dashboard_filters(), product_plot=product_json, card=card_data(year=year))
 
 
+def top_product_data(year):
+    df = GenerateData(year=2018).top_products()
+    print(year, 'sent year')
+    data = [go.Bar(x=df['product_name'].str.title().str.replace('_', ' '), y=df['number_ordered'])]
+    layout = go.Layout(xaxis=dict(tickangle=-50),title='Plot 1')
+    return go.Figure(data=data, layout=layout)
+
+def card_data(year) -> dict:
+    orders = GenerateData(year=2018).total_orders()
+    sales = GenerateData(year=year).total_sales()
+
+    return {'orders':orders, 'sales':sales}
+
+def dashboard_filters() -> dict:
+    year = FilterValues().year_filter()
+
+    return {'year':year}
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
